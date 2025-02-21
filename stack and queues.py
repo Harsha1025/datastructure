40286238822aca081c4f4365c55810568fe9ded2
Harsha <swag56379@gmail.com>
2025-02-22 05:10:10 +0530

    queus

--- stack and queues.py
+++ stack and queues.py
@@ -213,3 +213,159 @@ def sort_arr():
 
 print(stack_arr)
 sort_arr()
+
+
+
+# Queues
+
+
+# implement queues in python - using list
+
+my_list =["sunday","Monday","Tuesday","Wednesday","Thrusday","Friday","Saturday"]
+my_queue = []
+print(my_list)
+
+
+# adding elements in the stack
+
+for i in range(0,len(my_list)):
+    my_queue.append(my_list[i])
+    print(my_queue)
+
+# removing elements from the stack
+
+for i in range(0,len(my_queue)):
+    my_queue.pop(0)
+    print(my_queue)
+
+def showQueue(my_queue):
+    for i in my_queue:
+        print(i,end=" ")
+    print("\n ")
+
+# update the value in queue
+
+my_queue =[1,2,3,4,5,6,7,8,9]
+def update(data,value):
+    count = False
+    try :
+        for i in range(0,len(my_queue)):
+            if my_queue[i] == data:
+                my_queue[i] = value
+                count = True
+        if count == False:
+            raise MyCustomException("No data present in queue")
+    except MyCustomException as e :
+        print(data,"  ",e)
+    showQueue(my_queue)
+
+    return my_queue
+
+
+update(3,99)
+update(34,11)
+
+
+
+# implement queues in python - Using Array
+
+my_Queue = myarray.array('i',[1,2,3,4,5,6,7,8,9])
+
+# reading the Queue
+
+showQueue(my_Queue)
+
+
+# inserting elements in the stack
+
+my_Queue.append(11)
+showQueue(my_Queue)
+
+
+# deleting element from the queue
+
+for i in range(0,len(my_Queue)):
+    my_Queue.pop(0)
+    showQueue(my_Queue)
+
+my_Queue=myarray.array('i',[1,2,3,4,5,6,6,7,5,9])
+
+def update(data,value,my_queue):
+    ind =0
+    count = False
+    try:
+        ind=my_queue.index(data)
+        my_queue[ind] = value
+        showQueue(my_queue)
+    except ValueError:
+        print(data,": NO data found in the queue")
+
+update(4,88,my_Queue)
+update(34,2,my_Queue)
+
+
+
+
+# implement queue in python - Using Deque
+
+print(my_list)
+
+my_queue =deque()
+
+# inserting elements using append()
+
+for i in my_list:
+    my_queue.append(i)
+    showQueue(my_queue)
+
+# deleting element using popleft()
+for i in range(0,len(my_queue)):
+    my_queue.popleft()
+    showQueue(my_queue)
+
+
+# inserting elements using appendleft()
+
+for i in my_list:
+    my_queue.appendleft(i)
+    showQueue(my_queue)
+
+# removing using pop()
+
+for i in range(0,len(my_queue)):
+    my_queue.pop()
+    showQueue(my_queue)
+
+
+showQueue(my_Queue)
+
+update(1,34,my_Queue)
+update(45,3,my_Queue)
+
+
+# sorting a Queue
+queue_deq = deque([3,4,3,2,2,1,6,5,4,33,2])
+queue_arr = myarray.array('i',[4,3,2,4,56,67,3,2,1,4])
+queue_list=[4,5,6,7,8,54,4,3,2,21,1]
+
+sample=[]
+def sort(queue_deq):
+    for i in range(0,len(queue_deq)):
+        if type(queue_deq) == deque:
+         res = queue_deq.popleft()
+        else:
+                res = queue_deq.pop(0)
+        sample.append(res)
+    sample.sort()
+    showQueue(sample)
+print(queue_deq)
+
+
+sort(queue_deq)
+
+sample=[]
+print(queue_arr)
+sort(queue_arr)
+sample=[]
+print(queue_list)
+sort(queue_list)
